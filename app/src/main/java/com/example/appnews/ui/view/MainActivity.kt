1413306951893e002f8667236e766b5b7dad7731
JosianeSouzaCostaGoncalves <josiane.scg@gmail.com>
2024-11-29 10:26:22 -0300

    Deficao da estrutura de pastas

--- app/src/main/java/com/example/appnews/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/appnews/ui/view/MainActivity.kt
@@ -1,16 +1,9 @@
-package com.example.appnews
+package com.example.appnews.ui.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.appnews.ui.theme.AppNewsTheme
 
 class MainActivity : ComponentActivity() {
